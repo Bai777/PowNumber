2b2af7bf0674913ea3d2db90afcee3b8d6d1347a
bai_777 <bai_777@mail.ru>
2022-09-27 19:11:11 +0300

    реализовал возведение в степень с помощью рекурсии

---
+++ src/main/java/org/example/PowNumber.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class PowNumber {
+    public static int operationNumber(int x, int n) {
+        return n == 0 ? 1 : operationNumber(x, n - 1) * x;
+    }
+
+    public static void main(String[] args) {
+        Scanner myNumber = new Scanner(System.in);
+        System.out.print("Input number x = ");
+        int x = myNumber.nextInt();
+        System.out.print("Input pow n = ");
+        int n = myNumber.nextInt();
+        int pow = operationNumber(x, n);
+        System.out.println("Pow( " + x + ", " + n + ") = " + pow);
+    }
+}
